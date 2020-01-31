8888337768e2dba5ed439bd0568b5a6c0e52c9d7
vasilisdim <13-12698@saeinstitute.edu>
2020-01-31 21:21:26 +0200

    Made Challenge Manager

---
+++ StackEmMan!/Assets/Scripts/ChallengeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeManager : MonoBehaviour
+{
+    int numOfComponenetsPerClock;
+    int numOfClocks;
+    List<Clock> ClocksNeeded;
+    public bool CheckChallengeComplete;
+
+    void Start()
+    {
+        MakeClocksNeeded();
+    }
+
+    private void MakeClocksNeeded()
+    {
+        for (int i = 0; i < numOfClocks; i++)
+        {
+
+        }
+    }
+}

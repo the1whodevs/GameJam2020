7205264310a6a28059bff7708945c832f629903e
Maya <13-12680@saeinstitute.edu>
2020-02-01 14:28:55 +0200

    UI Manager

---
+++ StackEmMan!/Assets/Scripts/UIManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [SerializeField] private GameObject _p1Ready;
+    [SerializeField] private GameObject _p2Ready;
+
+    [SerializeField] private GameObject _p1PressStart;
+    [SerializeField] private GameObject _p2PressStart;
+
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+
+            _p1Ready.SetActive(false);
+            _p2Ready.SetActive(false);
+
+            _p1PressStart.SetActive(true);
+            _p2PressStart.SetActive(true);
+
+        }
+    }
+
+    public void EnablePlayer(int playerNum)
+    {
+
+
+        if (playerNum == 1)
+	    {
+
+            _p1Ready.SetActive(true);
+            _p1PressStart.SetActive(false);
+
+        }
+        else if (playerNum == 2)
+        {
+            _p2Ready.SetActive(true);
+            _p2PressStart.SetActive(false);
+        }
+    }
+    
+}

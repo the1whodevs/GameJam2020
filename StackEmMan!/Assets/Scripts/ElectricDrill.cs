1d890214f39bd81fa60268e1705165d7a67932fd
xarcharias <exarchakouolivia@gmail.com>
2020-02-01 12:57:55 +0200

    Created the Tool scripts
    Tool.cs
    Screwdriver.cs
    Hammer.cs
    ElectricDrill.cs

--- StackEmMan!/Assets/Scripts/ElectricDrill.cs
+++ StackEmMan!/Assets/Scripts/ElectricDrill.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Screwdriver : Tool
+public class ElectricDrill : Tool
 {
-    
+    [SerializeField] private int _maxBattery = 100;
+    [SerializeField] private int _currentBattery;
+
     public override void Use()
     {
         if (true)
--- StackEmMan!/Assets/Scripts/Hammer.cs
+++ StackEmMan!/Assets/Scripts/Hammer.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Screwdriver : Tool
-{
+public class Hammer : Tool { 
     
     public override void Use()
     {

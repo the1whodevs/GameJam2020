699947f6a42445446a5fafacc86b3ff8b92c0458
xarcharias <exarchakouolivia@gmail.com>
2020-02-01 13:35:52 +0200

    Progress on ElectricDrill code

--- StackEmMan!/Assets/Scripts/ElectricDrill.cs
+++ StackEmMan!/Assets/Scripts/ElectricDrill.cs
@@ -6,12 +6,27 @@ public class ElectricDrill : Tool
 {
     [SerializeField] private int _maxBattery = 100;
     [SerializeField] private int _currentBattery;
+    [SerializeField] float drillTimer = 10.0f;
+    
+    int itemInUse = false;
 
     public override void Use()
     {
-        if (true)
-        {
+        itemInUse = true;
+    }  
 
+    // Checks when the player is using the object and begin the timer
+    public void Update()
+    {
+        if (itemInUse == true)
+        {
+            drillTimer -= Time.deltaTime;
+
+            if (drillTimer < 0)
+            {
+                Debug.Log("Drill used for 10 seconds.");
+                itemInUse = false;
+            }
         }
     }
 }

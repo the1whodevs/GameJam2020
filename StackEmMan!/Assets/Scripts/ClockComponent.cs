916854336be94fa903c184442826ade2856f903d
MayaIsAPotato <13-12608@saeinstitute.edu>
2020-02-01 19:16:48 +0200

    Added ClockComponent script

--- StackEmMan!/Assets/Scripts/ClockComponent.cs
+++ StackEmMan!/Assets/Scripts/ClockComponent.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+public enum ComponentType { smallHand, bigHand, numbers, frame, bell, smallCog, mediumCog, bigCog}
 
 public class ClockComponent : MonoBehaviour
 {
-    
+    public ComponentType Type;
 }
